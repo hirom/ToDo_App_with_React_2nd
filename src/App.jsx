898a90bd880ac495124e78b5493476ecf537f74e
hirom <hirom0808@gmail.com>
2022-03-19 18:23:38 +0900

    useStateを用いた実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  const [imcompleteTodos, setImcompleteTodos] = useState([
+    "あああああ",
+    "いいいいい"
+  ]);
+
+  const [completeTodos, setCompleteTodos] = useState(["ううううう"]);
+
+  return (
+    <>
+      <div className="input-area">
+        <input id="add-text" placeholder="TODOを入力" />
+        <button id="add-button">追加</button>
+      </div>
+      <div className="imcompleted-area">
+        <p className="title">未完了のTodo</p>
+        <ul>
+          {imcompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="completed-area">
+        <p className="title">完了のTodo</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
